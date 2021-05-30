e2c33138e09749077cb79c2fc454a6e450ffa243
KUDO Shunsuke <kudo.shunsuke.xx@gmail.com>
2021-05-31 00:03:00 +0900

    swap player

--- src/index.tsx
+++ src/index.tsx
@@ -31,8 +31,8 @@ class Board extends React.Component<{}, BoardState> {
 
   handleClick(i: number) {
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
 
   renderSquare(i: number) {
@@ -45,7 +45,7 @@ class Board extends React.Component<{}, BoardState> {
   }
 
   render() {
-    const status = "Next player: X";
+    const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
 
     return (
       <div>
