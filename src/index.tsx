c528c0e998ebb7cb34784c0bb315e4184297fd65
KUDO Shunsuke <kudo.shunsuke.xx@gmail.com>
2021-05-31 00:14:56 +0900

    add win judgement function

--- src/index.tsx
+++ src/index.tsx
@@ -20,6 +20,29 @@ function Square(props: Props) {
   );
 }
 
+function calculateWinner(squares: string[]): string {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return "";
+}
+
 class Board extends React.Component<{}, BoardState> {
   constructor(props: {}) {
     super(props);
